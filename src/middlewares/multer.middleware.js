69ae2f389217d8d7720fcce17208867a0eadcf58
Sibsankar-de <sibsankarde910@gmail.com>
2024-06-19 15:43:12 +0530

    commit 5

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,16 @@
 import multer from "multer"
+import path from "path"
+import { fileURLToPath } from 'url';
+
+// Convert import.meta.url to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const tempDir = path.join(__dirname, '../..', 'public', 'temp');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/temp')
+        cb(null, tempDir)
     },
     filename: function (req, file, cb) {
 
