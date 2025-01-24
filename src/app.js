4a80a9d0f82756d8e798ac15389cbced8a4a1cb6
hostelbetting <hostelbetting.fun@gmail.com>
2025-01-24 13:37:26 +0530

    Update dependencies and add ping route

--- src/app.js
+++ src/app.js
@@ -13,7 +13,10 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
-// function calls
+// non cors routes
+app.get("/ping", (req, res) => {
+    res.send("Ping successful");
+});
 
 // Router imports
 import userRouter from "./routes/user.routes.js"
